8c760a5cec7576eb1c99afae22c6f10586045038
Foad Ramezani <f.ramezani@khallagh.com>
2021-08-29 14:44:12 +0430

    Upload Configuring files for Zabbix

---
+++ Telegram Scripts/zbxtg_settings.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+tg_key = "185421830:AAEGK0ctgD7odQ0YkN7o7U8bcvlVxDsRUDE"  # telegram bot api key
+
+zbx_tg_prefix = "zbxtg"  # variable for separating text from script info
+zbx_tg_tmp_dir = "/tmp/" + zbx_tg_prefix  # directory for saving caches, uids, cookies, etc.
+zbx_tg_signature = False
+
+zbx_server = "http://192.168.102.124"  # zabbix server full url
+zbx_api_user = "Admin"
+zbx_api_pass = "zabbix"
+zbx_api_verify = False  # True - do not ignore self signed certificates, False - ignore
+
+proxy_to_zbx = None
+proxy_to_tg = None
+
+#proxy_to_zbx = "proxy.local:3128"
+#proxy_to_tg = "proxy.local:3128"
+
+emoji_map = {
+    "ok": "✅",
+    "problem": "❗",
+    "info": "ℹ️",
+    "warning": "⚠️",
+    "disaster": "❌",
+    "bomb": "💣",
+    "fire": "🔥",
+    "hankey": "💩",
+}
